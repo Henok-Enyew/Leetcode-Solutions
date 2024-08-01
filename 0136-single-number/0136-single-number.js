18a4de1397072f30d3b43632337138ef0166791b
Henok-Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2024-08-01 20:21:53 +0300

    Time: 64 ms (47.45%), Space: 52.1 MB (44.17%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  let uniqueNum = nums[0];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i = i + 2) {
+    if (nums[i] !== nums[i + 1]) {
+      uniqueNum = nums[i];
+      break;
+    }
+  }
+  return uniqueNum;
+};
