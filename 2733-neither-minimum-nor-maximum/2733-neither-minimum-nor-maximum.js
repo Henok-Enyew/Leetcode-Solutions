36f2b81cab48c1a8fdfadff700495be19cd4cc06
Henok-Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2024-08-03 01:37:06 +0300

    Time: 147 ms (47.74%), Space: 58.9 MB (86.01%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.js
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var findNonMinOrMax = function(nums) {
-  nums.sort((a, b) => a - b);
   if (nums.length < 3) return -1;
+  nums.sort((a, b) => a - b);
   return nums[1];
 };
