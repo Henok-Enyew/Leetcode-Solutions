561c05d4801d11c528fd5834ae5115408eb809f5
Henok-Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2024-08-20 00:15:28 +0300

    Time: 155 ms (51.52%), Space: 18.8 MB (56.11%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        answer = []
+        for num in nums:
+            answer.append(num * num)
+        return sorted(answer)
