9fa6af1375d81ec775737f94557c2acd6b300f2d
Henok-Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2024-08-24 21:50:00 +0300

    Time: 553 ms (65.60%), Space: 34.6 MB (47.46%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -6,24 +6,16 @@ class Solution:
         start = 0
 
         for end in range(len(nums)):
-            # Add current number to the map and window sum
             if nums[end] in map:
-                # If a duplicate is found within the current window
                 while nums[end] in map:
                     del map[nums[start]]
                     windowSum -= nums[start]
                     start += 1
-            
             map[nums[end]] = end
             windowSum += nums[end]
-
-            # Check if window size equals k
             if end - start + 1 == k:
-                maxSum = max(maxSum, windowSum)
-                
-                # Slide the window
+                maxSum = max(maxSum, windowSum)                
                 windowSum -= nums[start]
                 del map[nums[start]]
                 start += 1
-        
         return maxSum
