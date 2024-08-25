c37d81cbc3ef6f8116935a7f95c7640e49ffe8d1
Henok-Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2024-08-25 17:13:44 +0300

    Time: 44 ms (58.10%), Space: 16.5 MB (74.72%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.py
@@ -3,20 +3,17 @@ class Solution:
         max_length = 0
         frequency = {}
         left = 0
-
-        for right in range(len(s)):
-            frequency[s[right]] = frequency.get(s[right], 0) + 1
-
-            while frequency[s[right]] > 2:
+        for i,right in enumerate(s):
+            if right not in frequency:
+                frequency[right] = 1
+            elif right in frequency:
+                frequency[right] += 1
+            while frequency[right] >2:
                 frequency[s[left]] -= 1
-
-                # Delete leftmost char.
+                
                 if frequency[s[left]] == 0:
                     del frequency[s[left]]
-
-                left += 1
-            
-            max_length = max(max_length, right - left + 1)
-
+                left+=1
+            max_length = max(max_length, i-left+1)
         return max_length
-
+                
