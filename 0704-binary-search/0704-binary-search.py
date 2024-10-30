af902ce48c1a58f5c20ee8c4248173f5d5007dc4
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2024-10-30 20:16:04 +0300

    Time: 12 ms (5.28%), Space: 17.9 MB (81.37%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,6 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
+        map_ = {num: index for index, num in enumerate(nums)}
+        if target in map_.keys():
+            return map_[target]
         else:
             return -1
