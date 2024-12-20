8ba20729f3fe56ab6216ad5307d511ca71fcc04e
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2024-12-20 09:56:00 +0300

    Time: 14 ms (85.94%), Space: 23 MB (5.08%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -7,11 +7,9 @@ class Solution:
             if level % 2 == 1:
                 left.val, right.val = right.val, left.val
             
-            # Recursively process the next level
             dfs(left.left, right.right, level + 1)
             dfs(left.right, right.left, level + 1)
         
-        # Start DFS with the root's children at level 1
         if root:
             dfs(root.left, root.right, 1)
         return root
