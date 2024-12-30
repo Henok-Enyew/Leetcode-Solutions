0f8125f1fc072baa022afc0d08f6379fb6291afa
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2024-12-30 13:19:59 +0300

    Time: 79 ms, Memory: 21.3 MB - LeetHub

--- 1928-178-2466-count-ways-to-build-good-strings/1928-178-2466-count-ways-to-build-good-strings.py
+++ 1928-178-2466-count-ways-to-build-good-strings/1928-178-2466-count-ways-to-build-good-strings.py
@@ -2,18 +2,15 @@ class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
         MOD = 10**9 + 7
         
-        # dp[i] will store the number of ways to form strings of length i
         dp = [0] * (high + 1)
-        dp[0] = 1  # Base case: one way to create an empty string
+        dp[0] = 1  
         
-        # Fill dp array
         for length in range(1, high + 1):
             if length >= zero:
                 dp[length] = (dp[length] + dp[length - zero]) % MOD
             if length >= one:
                 dp[length] = (dp[length] + dp[length - one]) % MOD
 
-        # Sum up the counts for lengths between `low` and `high`
         result = 0
         for length in range(low, high + 1):
             result = (result + dp[length]) % MOD
