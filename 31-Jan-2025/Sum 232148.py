7d0018c00cfbe5ba31e92c998857d56373671e0b
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-01-31 21:56:05 +0300

    Created 'Sum' by A2SV Hub

---
+++ 31-Jan-2025/Sum 232148.py
@@ -0,0 +1,14 @@
+# Problem: Sum - https://codeforces.com/contest/1742/problem/A
+
+import sys
+input = sys.stdin.read
+
+data = input().splitlines()
+n = int(data[0])
+
+for i in range(1, n + 1):
+    a, b, c = map(int, data[i].split())
+    if a + b == c or a + c == b or b + c == a:
+        print("YES")
+    else:
+        print("NO")
