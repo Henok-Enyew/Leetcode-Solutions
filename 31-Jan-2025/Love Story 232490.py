ce4f1d7d0f809b03fa311d84d308e681ba33162f
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-01-31 22:52:01 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 31-Jan-2025/Love Story 232490.py
@@ -0,0 +1,14 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+t = int(input())
+while(t > 0):
+    t -= 1
+    story = input()
+    codeforces = "codeforces"
+    counter = 0
+    
+    for i in range(10):
+        if story[i] != codeforces[i]:
+            counter += 1
+
+    print(counter)
