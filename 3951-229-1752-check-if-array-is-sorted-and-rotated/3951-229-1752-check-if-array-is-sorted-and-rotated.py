54db0ff17ac88fed4fdfef951a5045e3054a85f7
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-02 23:07:43 +0300

    Time: N/A, Memory: N/A',
      },
      questionId: '1878',
      submissionId: '1529034375',
      sessionId: '9145085',
      getLangDisplay: 'python3',
      submissionCode: '\u000Aclass Solution:\u000A    def check(self, nums: List[int]) \u002D\u003E bool:\u000A        count \u003D sum(nums[i] \u003E nums[(i + 1) % len(nums)] for i in range(len(nums)))\u000A        return count \u003C\u003D 1\u000A\u000A\u000A\u000A\u000A\u000A\u000A',
      editCodeUrl: '/problems/check-if-array-is-sorted-and-rotated/',
      checkUrl: '/submissions/detail/1529034375/check/',
      runtimeDistributionFormatted: '{\u0022lang\u0022: \u0022python3\u0022, \u0022distribution\u0022: [[\u00220\u0022, 86.7555], [\u00221\u0022, 2.0795], [\u00222\u0022, 1.2518], [\u00223\u0022, 4.8657], [\u00224\u0022, 1.9382]]}',
      memoryDistributionFormatted: '{\u0022lang\u0022: \u0022python3\u0022, \u0022distribution\u0022: [[\u002216900\u0022, 0.0202], [\u002217000\u0022, 0.2019], [\u002217100\u0022, 1.4335], [\u002217200\u0022, 4.26], [\u002217300\u0022, 4.3004], [\u002217400\u0022, 3.5938], [\u002217500\u0022, 4.8455], [\u002217600\u0022, 9.6103], [\u002217700\u0022, 23.1173], [\u002217800\u0022, 18.514], [\u002217900\u0022, 16.4345]]}',
      langs: [
        
      ],
      runtime: 'None',
      memory: 'None',
      enableMemoryDistribution: 'True',
      nonSufficientMsg: 'Sorry. We do not have enough accepted submissions to show distribution chart. - LeetHub

---
+++ 3951-229-1752-check-if-array-is-sorted-and-rotated/3951-229-1752-check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        count = sum(nums[i] > nums[(i + 1) % len(nums)] for i in range(len(nums)))
+        return count <= 1
+
+
+
+
+
+
