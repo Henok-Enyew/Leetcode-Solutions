90b7005f4fb74bf88b5f18aade17741e68ddd3c5
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-03 21:14:39 +0300

    Created 'Team' by A2SV Hub

---
+++ 03-Feb-2025/Team 237257.py
@@ -0,0 +1,14 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+t = int(input())
+questions_answered = 0
+while(t > 0):
+    t -= 1
+    team = input().split()
+    sures = 0
+    for i in team:
+        if i == '1':
+            sures+=1
+    if sures >= 2:
+        questions_answered += 1
+print(questions_answered)
