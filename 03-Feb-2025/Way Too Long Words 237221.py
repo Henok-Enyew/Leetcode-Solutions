f40d9b03fdd371028d5f510fb277204cf4f0c4b1
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-03 21:08:56 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 03-Feb-2025/Way Too Long Words 237221.py
@@ -0,0 +1,10 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+t = int(input())
+while(t > 0):
+    t -= 1
+    word = input()
+    if len(word) <= 10:
+        print(word)
+    else:
+        print(f"{word[0]}{len(word)-2}{word[len(word)-1]}")
