8f194dc6a6f70528fbb55873258bb5d6cb06e2c7
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-05 23:15:14 +0300

    Created 'Presents' by A2SV Hub

---
+++ 05-Feb-2025/Presents 245121.py
@@ -0,0 +1,10 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+t = int(input())
+presents = list(map(int, input().split()))  
+output = [0] * t  
+
+for i in range(t):
+    output[presents[i] - 1] = i + 1  
+
+print(*output)  
