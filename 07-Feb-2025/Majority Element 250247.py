c281ff2594136d94a92bc62b9351e47c095e0e69
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-07 18:39:07 +0300

    Created 'Majority Element' by A2SV Hub

---
+++ 07-Feb-2025/Majority Element 250247.py
@@ -0,0 +1,13 @@
+# Problem: Majority Element - https://leetcode.com/problems/majority-element/description/
+
+class Solution:
+    def majorityElement(self, nums: list[int]) -> int:
+        map = {}
+        for i,v in enumerate(nums):
+            if v in map.keys():
+                map[v] += 1
+            else:
+                map[v] = 1
+        for k,v in map.items():
+            if(v > len(nums) / 2):
+                return k
