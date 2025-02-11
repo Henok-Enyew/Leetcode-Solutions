485d5637c54c81bb47e957a635278af398782efe
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-11 22:58:31 +0300

    Created 'Same Differences' by A2SV Hub

---
+++ 11-Feb-2025/Same Differences 258247.py
@@ -0,0 +1,16 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+from collections import Counter 
+t = int(input())
+while t:
+    t-=1
+    length = int(input())
+    numbers = list(map(int, input().split()))
+    cnt = Counter()
+    ans = 0
+    for i, x in enumerate(numbers):
+        ans += i - cnt[i - x]
+        cnt[i - x] += 1
+    n = len(numbers)
+    ans = (n * (n - 1) // 2) - ans
+    print(ans)
