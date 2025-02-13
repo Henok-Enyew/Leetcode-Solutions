83275a8697bb5acc00e0295398dbf047d6cefee9
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-13 19:40:52 +0300

    Created 'Isomorphic Strings' by A2SV Hub

---
+++ 13-Feb-2025/Isomorphic Strings 263513.py
@@ -0,0 +1,15 @@
+# Problem: Isomorphic Strings - https://leetcode.com/problems/isomorphic-strings/
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map = {}
+        if(len(s) != len(t)):
+            return False
+        for i in range(len(s)):
+            if s[i] in map.keys():
+                if map[s[i]] != t[i]:
+                    return False
+            elif t[i] in map.values():
+                    return False
+            map[s[i]] = t[i]
+        return True
