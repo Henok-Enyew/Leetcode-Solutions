21d9e13c731081bbc8f9f9c7b8ef836fa95a4535
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-17 22:12:36 +0300

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/C - Barking Password 269678.py
@@ -0,0 +1,25 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+def can_unlock(password, words):
+    first_letter_match = False
+    second_letter_match = False
+
+    for word in words:
+        if word == password:  
+            print("YES")
+            return
+        if word[1] == password[0]:  
+            first_letter_match = True
+        if word[0] == password[1]: 
+            second_letter_match = True
+
+    if first_letter_match and second_letter_match:
+        print("YES")
+    else:
+        print("NO")
+
+password = input().strip()
+n = int(input().strip())
+words = [input().strip() for _ in range(n)]
+
+can_unlock(password, words)
