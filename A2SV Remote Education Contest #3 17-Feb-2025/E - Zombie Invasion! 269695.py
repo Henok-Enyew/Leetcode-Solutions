778d3a0cdcb73036f32b7408595462e348956d6a
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-17 22:15:11 +0300

    Created 'E - Zombie Invasion!' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/E - Zombie Invasion! 269695.py
@@ -0,0 +1,32 @@
+# Problem: E - Zombie Invasion! - https://codeforces.com/gym/588094/problem/E
+
+from collections import defaultdict
+t = 1
+t = int(input())
+while t:
+    t -= 1  
+    n, k = input().split()
+    n = int(n)
+    k = int(k)
+
+    a = list(map(int, input().split()))
+    x = list(map(int, input().split()))
+
+    dic = defaultdict(int) 
+
+    for i in range(n):
+        dic[abs(x[i])] += a[i] 
+
+    f = 1
+    left = 0
+    for i in range(1, n + 1):  
+        left += k - dic.get(i, 0)
+
+        if left < 0:
+            f = 0
+            break
+
+    if f:
+        print("YES")
+    else:
+        print("NO")
