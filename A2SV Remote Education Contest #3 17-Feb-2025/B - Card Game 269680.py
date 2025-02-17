df24999a5fc593b09b92e64d41d5a4e7e9a2ae5a
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-17 22:12:40 +0300

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/B - Card Game 269680.py
@@ -0,0 +1,18 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+from collections import Counter
+
+cards = int(input())
+card = list(map(int, input().split()))
+sm = sum(card)
+partition = int(sm / int(cards/2))
+mp = Counter()
+# print(sm, partition)
+visited = []
+for i in range(cards):
+    for j in range(cards):
+        if i not in visited and j not in visited and i!=j and card[i] + card[j] == partition:
+            print(f"{i+1} {j+1}")
+            visited.append(i)
+            visited.append(j)
+            continue
