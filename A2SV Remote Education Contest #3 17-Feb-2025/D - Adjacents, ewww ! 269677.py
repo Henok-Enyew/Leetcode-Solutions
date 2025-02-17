3eed864fb9a394eac48ebceb4cbe288a2fc0f5d6
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-17 22:12:24 +0300

    Created 'D - Adjacents, ewww !' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/D - Adjacents, ewww ! 269677.py
@@ -0,0 +1,22 @@
+# Problem: D - Adjacents, ewww ! - https://codeforces.com/gym/588094/problem/D
+
+def generate_matrix(n):
+    if n == 2:
+        print(-1)
+        return
+    matrix = [[0] * n for _ in range(n)]
+    odd_numbers = list(range(1, n*n + 1, 2))
+    even_numbers = list(range(2, n*n + 1, 2))
+    
+    numbers = odd_numbers + even_numbers 
+    index = 0
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = numbers[index]
+            index += 1
+    for row in matrix:
+        print(*row)
+t = int(input().strip())
+for _ in range(t):
+    n = int(input().strip())
+    generate_matrix(n)
