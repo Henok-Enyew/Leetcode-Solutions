becba8ad12f9dec1bdbc7d9b1f0405af93411307
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-17 22:12:50 +0300

    Created 'A - Nathan is rich' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/A - Nathan is rich 269682.py
@@ -0,0 +1,10 @@
+# Problem: A - Nathan is rich - https://codeforces.com/gym/588094/problem/A
+
+t = int(input())
+while t:
+    t-=1
+    wheels = int(input())
+    minimum_wheels = wheels//4
+    if wheels%4!=0:
+        minimum_wheels+=1
+    print(minimum_wheels)
