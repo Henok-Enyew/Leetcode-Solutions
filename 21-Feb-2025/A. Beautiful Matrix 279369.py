b806ebc2c2140c7455ab5979ede365878fbbb5ae
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-21 21:23:51 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 21-Feb-2025/A. Beautiful Matrix 279369.py
@@ -0,0 +1,10 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+arrays = [list(map(int, input().split())) for _ in range(5)]
+for i in range(5):
+    for j in range(5):
+        if arrays[i][j] == 1:
+            row = i+1
+            column = j+1
+moves = abs(row-3) + abs(column-3)
+print(moves)
