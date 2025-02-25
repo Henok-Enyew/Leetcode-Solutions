f17e5212f75fdedc4764272d43aff330abb3329b
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-25 23:12:20 +0300

    Created 'Maximum Product of Word Lengths' by A2SV Hub

---
+++ 25-Feb-2025/Maximum Product of Word Lengths 282815.py
@@ -0,0 +1,22 @@
+# Problem: Maximum Product of Word Lengths - https://leetcode.com/problems/maximum-product-of-word-lengths/
+
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        l = len(words)
+        sets = [None] * l
+        lengths = [None] * l
+
+        def get_set(c):
+            s = set()
+            for t in c:
+                s.add(t)
+            return s
+        for i in range(l):
+            sets[i] = get_set(words[i])
+            lengths[i] = len(words[i])
+        best = 0
+        for i in range(l):
+            for j in range(l):
+                if len(sets[i] & sets[j]) == 0:
+                    best = max(best,lengths[i] * lengths[j])
+        return best
