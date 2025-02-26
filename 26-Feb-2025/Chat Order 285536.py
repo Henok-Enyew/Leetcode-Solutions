c35da99b699bcbf3e83a684a692cea064ca6fe31
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-02-26 23:18:12 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 26-Feb-2025/Chat Order 285536.py
@@ -0,0 +1,15 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+t = int(input())
+
+stack = []
+store = set()
+while t:
+    t-=1
+    friend = input()
+    stack.append(friend)
+
+for i in range(len(stack)-1,-1,-1):
+    if not stack[i] in store:
+        print(stack[i])
+        store.add(stack[i])
