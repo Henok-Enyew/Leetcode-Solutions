cfc54721633be1aad3d135f35a3d694a0e774ee8
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-04 20:54:22 +0300

    Created 'Sort the People' by A2SV Hub

---
+++ 04-Mar-2025/Sort the People 292104.py
@@ -0,0 +1,12 @@
+# Problem: Sort the People - https://leetcode.com/problems/sort-the-people/
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        map = {}
+        output = []
+        for i in range(len(names)):
+            map[heights[i]] = names[i]
+        heights.sort(reverse=True)
+        for h in heights:
+            output.append(map[h])
+        return output
