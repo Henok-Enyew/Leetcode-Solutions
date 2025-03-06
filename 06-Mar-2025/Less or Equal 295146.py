39ed05459267e576cb9506678d56be4c1a0347a7
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-06 21:17:06 +0300

    Created 'Less or Equal' by A2SV Hub

---
+++ 06-Mar-2025/Less or Equal 295146.py
@@ -0,0 +1,18 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+n, k = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+numbers.sort()
+
+if k == 0:
+    if numbers[0] > 1:
+        print(numbers[0] - 1)
+    else:
+        print(-1)
+else:
+    x = numbers[k - 1]  
+    if k < n and numbers[k] == x:
+        print(-1)
+    else:
+        print(x)
