da952be0e4262abb14782e835bee8940488287cf
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-06 19:28:46 +0300

    Created 'Merge Intervals (Optional)' by A2SV Hub

---
+++ 06-Mar-2025/Merge Intervals (Optional) 294953.py
@@ -0,0 +1,19 @@
+# Problem: Merge Intervals (Optional) - https://leetcode.com/problems/merge-intervals/
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        def pickFirst(val):
+            return val[0]
+        i = 0
+        answer = []
+        intervals.sort(key=pickFirst)
+        while i < len(intervals):
+            start = intervals[i][0]
+            end = intervals[i][1]
+            i += 1
+            while i < len(intervals) and end >= intervals[i][0] :           
+                start = min(start, intervals[i][0])
+                end = max(end, intervals[i][1])
+                i+=1
+            answer.append([start, end])
+        return answer
