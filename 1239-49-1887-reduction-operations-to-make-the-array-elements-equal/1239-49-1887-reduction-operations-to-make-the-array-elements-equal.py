145f76a936d65e5a9e4fe9781ca7ab6cd4dd0261
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-07 21:50:34 +0300

    Time: 147 ms, Memory: 23.3 MB - LeetHub

---
+++ 1239-49-1887-reduction-operations-to-make-the-array-elements-equal/1239-49-1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        largest = nums[0] 
+        i=1
+        count = 1
+        if nums[0] == nums[len(nums)-1]:
+            return 0
+        while i<len(nums)-1 and nums[i] != nums[len(nums)-1]:
+            if nums[i] == largest:
+                count += 1
+            else:
+                count = count + i+1
+                largest = nums[i]
+            i+=1 
+        return count
