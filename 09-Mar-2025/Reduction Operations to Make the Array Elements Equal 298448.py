1a7e65ccd44f24f3bfb753d6c0089933726b4be6
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-09 10:02:25 +0300

    Created 'Reduction Operations to Make the Array Elements Equal' by A2SV Hub

---
+++ 09-Mar-2025/Reduction Operations to Make the Array Elements Equal 298448.py
@@ -0,0 +1,18 @@
+# Problem: Reduction Operations to Make the Array Elements Equal - https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/
+
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        larg = nums[0] 
+        i=1
+        count = 1
+        if nums[0] == nums[len(nums)-1]:
+            return 0
+        while i<len(nums)-1 and nums[i] != nums[len(nums)-1]:
+            if nums[i] == larg:
+                count += 1
+            else:
+                count = count + i+1
+                larg = nums[i]
+            i+=1 
+        return count
