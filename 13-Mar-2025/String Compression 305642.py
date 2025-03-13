1063bd39ae6d649d43f6cc750cfde057c92d2623
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-13 21:16:57 +0300

    Created 'String Compression' by A2SV Hub

---
+++ 13-Mar-2025/String Compression 305642.py
@@ -0,0 +1,26 @@
+# Problem: String Compression - https://leetcode.com/problems/string-compression/
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        left,right,count = 0,0,0
+        while right < len(chars):
+            # left=right
+            chars[left] = chars[right]
+            while right < len(chars) and chars[left] == chars[right]:
+                count+=1
+                right+=1
+            left+=1
+            if count>1:
+                if count > 9:
+                    st = str(count)
+                    for s in st:
+                        chars[left] = s
+                        print(chars[left])
+                        left+=1
+                    
+                else:
+                    chars[left] = str(count)
+                    left+=1
+            count=0
+            # left+=1
+        return left
