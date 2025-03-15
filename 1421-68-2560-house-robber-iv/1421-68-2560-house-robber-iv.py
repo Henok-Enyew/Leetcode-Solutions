5d6007bac9862bb94f2a402e8b4fed0dbb61ff47
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-15 19:30:04 +0300

    Time: 368 ms, Memory: 28.6 MB - LeetHub

---
+++ 1421-68-2560-house-robber-iv/1421-68-2560-house-robber-iv.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        left, right = min(nums), max(nums)
+        
+        def can_rob_at_least_k(mid: int) -> bool:
+            count, i = 0, 0
+            while i < len(nums):
+                if nums[i] <= mid:  
+                    count += 1
+                    i += 1  
+                i += 1  
+            return count >= k
+        
+        while left < right:
+            mid = (left + right) // 2
+            if can_rob_at_least_k(mid):
+                right = mid  
+            else:
+                left = mid + 1  
+        
+        return left
