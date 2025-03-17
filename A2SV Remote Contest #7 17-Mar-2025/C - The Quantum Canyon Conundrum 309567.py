13a0a36a662de72c9c25215e837200b7cd9bc724
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-17 20:55:30 +0300

    Created 'C - The Quantum Canyon Conundrum' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/C - The Quantum Canyon Conundrum 309567.py
@@ -0,0 +1,31 @@
+# Problem: C - The Quantum Canyon Conundrum - https://codeforces.com/gym/596004/problem/C
+
+def is_true_canyon(n, a):
+    segments = []
+    i = 0
+    while i < n:
+        current = a[i]
+        start = i
+        while i < n and a[i] == current:
+            i += 1
+        end = i - 1
+        segments.append((start, end, current))
+    
+    valid_segments = []
+    for seg in segments:
+        l, r, val = seg
+        left_cond = (l == 0) or (a[l - 1] > val)
+        right_cond = (r == n - 1) or (a[r + 1] > val)
+        if left_cond and right_cond:
+            valid_segments.append(seg)
+    
+    return len(valid_segments) == 1
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    if is_true_canyon(n, a):
+        print("YES")
+    else:
+        print("NO")
