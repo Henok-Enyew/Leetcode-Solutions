b63334d467dc529ad4ad48eb35c618b177974efe
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-17 21:04:35 +0300

    Created 'D - The Snap of Fury' by A2SV Hub

---
+++ A2SV Remote Contest #7 17-Mar-2025/D - The Snap of Fury 309579.py
@@ -0,0 +1,31 @@
+# Problem: D - The Snap of Fury - https://codeforces.com/gym/596004/problem/D
+
+n = int(input())
+L = list(map(int, input().split()))
+
+intervals = []
+for i in range(n):
+    a = max(0, i - L[i])
+    b = i - 1
+    if a <= b:
+        intervals.append((a, b))
+
+intervals.sort()
+merged = []
+for start, end in intervals:
+    if not merged:
+        merged.append((start, end))
+    else:
+        last_start, last_end = merged[-1]
+        if start <= last_end + 1:
+            new_start = last_start
+            new_end = max(last_end, end)
+            merged[-1] = (new_start, new_end)
+        else:
+            merged.append((start, end))
+
+covered = 0
+for start, end in merged:
+    covered += end - start + 1
+
+print(n - covered)
