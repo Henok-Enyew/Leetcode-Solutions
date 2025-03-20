a2d1e91070244ad7505e8c30c70784f88a93feac
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-21 01:33:16 +0300

    Created 'fruit-into-baskets' by A2SV Hub

---
+++ 20-Mar-2025/fruit-into-baskets 313920.py
@@ -0,0 +1,21 @@
+# Problem: fruit-into-baskets - https://leetcode.com/problems/fruit-into-baskets/
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        N = len(fruits)
+        left = 0
+        mp = {}
+        answer = 0
+        for right in range(N):
+            if fruits[right] not in mp:
+                mp[fruits[right]] = 0
+            mp[fruits[right]] += 1
+            
+            while len(mp) > 2:
+                mp[fruits[left]] -= 1
+                if mp[fruits[left]] == 0:
+                    del mp[fruits[left]]
+                left += 1
+            
+            answer = max(answer, right - left + 1)
+        return answer
