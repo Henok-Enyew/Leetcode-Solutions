a9dc3bfce9661bec9580ecd2362451ef80a6ff21
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-29 22:26:52 +0300

    Created 'Maximum subarray' by A2SV Hub

---
+++ 29-Mar-2025/Maximum subarray 321700.py
@@ -0,0 +1,12 @@
+# Problem: Maximum subarray - https://leetcode.com/problems/maximum-subarray/
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = nums[0]
+        curSum = 0
+        for i in range(len(nums)):
+            if curSum < 0:
+                curSum = 0
+            curSum += nums[i]
+            maxSum = max(maxSum, curSum)
+        return maxSum
