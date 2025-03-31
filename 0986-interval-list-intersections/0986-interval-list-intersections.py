276d69a5eb399b78bba59e4f8cb03633b161436b
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-03-31 20:10:57 +0300

    Time: 214 ms (5.29%), Space: 18.8 MB (23.55%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -0,0 +1,10 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        answer = []
+        for f in firstList:
+            for s in secondList:
+                if f[1] >= s[0] and s[1] >= f[0]:
+                    lower = max(f[0], s[0])
+                    upper = min(f[1], s[1])
+                    answer.append([lower,upper])
+        return answer
