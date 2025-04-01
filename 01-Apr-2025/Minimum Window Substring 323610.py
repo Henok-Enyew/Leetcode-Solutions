afbe0daa2931f8c34f5a78d7e859e588c0136535
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-01 23:14:48 +0300

    Created 'Minimum Window Substring' by A2SV Hub

---
+++ 01-Apr-2025/Minimum Window Substring 323610.py
@@ -0,0 +1,26 @@
+# Problem: Minimum Window Substring - https://leetcode.com/problems/minimum-window-substring/submissions/
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == '': return ''
+
+        countT = collections.Counter(t)
+        window = {}
+        have, need = 0, len(countT)
+        res,resLen = [-1,-1] , float('inf')
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c,0)
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l,r]
+                    resLen = r - l + 1
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l,r = res
+        return s[l:r+1] if resLen != float('inf') else ""
