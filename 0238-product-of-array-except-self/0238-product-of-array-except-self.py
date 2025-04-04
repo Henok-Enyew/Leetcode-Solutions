22ca2435cae55895a73f7882f50f460bb221211a
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-04 22:32:05 +0300

    Time: 35 ms (26.41%), Space: 26 MB (31.85%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,23 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        length = len(nums)
-        rightProduct = [1] * (length + 1)
-        leftProduct = [1] * (length +1)
-        answer = [1] * (length)
-        r = length -1
-        l = 1
-        for i,num in enumerate(nums):
-            leftProduct[l] = num *  leftProduct[l -1 ] 
-            rightProduct[r] = nums[r] *  rightProduct[r + 1 ] 
-            l+=1
-            r-=1
-        leftProduct = leftProduct[1:]
-        rightProduct = rightProduct[:-1]
-        for i,ans in enumerate(answer):
-            if(i==0):
-                answer[i] = rightProduct[i+1]
-            elif i==length-1:
-                answer[i] = leftProduct[i-1]
-            else:
-                answer[i] = rightProduct[i+1] * leftProduct[i-1]
+        suffix , prefix, answer = [], [], []
+        product = 1
+        for n in nums:
+            product *= n
+            prefix.append(product)
+        product = 1
+        for i in range(len(nums)-1,-1,-1):
+            product *= nums[i]
+            suffix.append(product)
+        suffix.reverse()
+        answer.append(suffix[1])
+        for i in range(1,len(nums)-1):
+            ans = prefix[i-1] * suffix[i + 1]
+            answer.append(ans)
+        answer.append(prefix[-2])
         return answer
