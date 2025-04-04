fdad961aa46fae21ad22d1158be65a1b1faf8ecf
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-04 21:54:38 +0300

    Time: 166 ms (10.19%), Space: 25.3 MB (6.28%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        squares = {}
+        i = 0
+        while i**2 <= c:
+            squares[i**2] = i
+            if c - i**2 in squares:
+                return True
+            i+=1
+        return False
