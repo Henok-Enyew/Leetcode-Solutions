4572fa11e484e4d648c1a18e0fc689ec477829a7
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-10 21:12:20 +0300

    Created 'Sereja and Dima' by A2SV Hub

---
+++ 10-Apr-2025/Sereja and Dima 331130.py
@@ -0,0 +1,22 @@
+# Problem: Sereja and Dima - https://codeforces.com/problemset/problem/381/A
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+l = serja = dima  = 0
+r = n - 1
+flag = 0
+while l <= r:
+    mx = 0
+    if arr[l] >= arr[r]:
+        mx = arr[l]
+        l += 1
+    else:
+        mx = arr[r]
+        r -= 1
+    if flag %2 ==0:
+        serja += mx
+    else:
+        dima += mx
+    flag += 1
+print(serja,dima)
