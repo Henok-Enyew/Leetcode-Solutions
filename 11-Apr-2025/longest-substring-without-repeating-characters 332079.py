7de0ae7828a8328e5c502475a769042b5a0f0c5c
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-11 21:00:57 +0300

    Created 'longest-substring-without-repeating-characters' by A2SV Hub

---
+++ 11-Apr-2025/longest-substring-without-repeating-characters 332079.py
@@ -0,0 +1,13 @@
+# Problem: longest-substring-without-repeating-characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mp = {}
+        max_len = 0
+        stt = 0
+        for i,st in enumerate(s):
+            if st in mp and mp[st] >= stt:
+                stt = mp[st]+1
+            mp[st] = i 
+            max_len = max(max_len, i-stt+1)
+        return max_len
