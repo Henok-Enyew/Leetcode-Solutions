13cfe141134c6e729a23259349ba11cceb0fa17b
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-11 21:31:34 +0300

    Created 'Find All Anagrams in a String ' by A2SV Hub

---
+++ 11-Apr-2025/Find All Anagrams in a String  332157.py
@@ -0,0 +1,12 @@
+# Problem: Find All Anagrams in a String  - https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        answer = []
+        l = len(p)-1
+        p_set =collections.Counter(p)
+        for i in range(l-1,len(s)):
+            s_set = collections.Counter(s[i-l:i+1])
+            if s_set == p_set:
+                answer.append(i-l)
+        return answer
