7509e7554e7b0ae7ca4e514502094c4c642d09ba
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-14 23:08:14 +0300

    Created 'Kefa and Company' by A2SV Hub

---
+++ 14-Apr-2025/Kefa and Company 334002.py
@@ -0,0 +1,23 @@
+# Problem: Kefa and Company - https://codeforces.com/problemset/problem/580/B
+
+n, d = map(int, input().split())
+friends = []
+for _ in range(n):
+    m, s = map(int, input().split())
+    friends.append((m, s))
+
+friends.sort()
+prefix = [0] * (n + 1)
+for i in range(n):
+    prefix[i+1] = prefix[i] + friends[i][1]
+
+max_friendship = 0
+left = 0
+for right in range(n):
+    while friends[right][0] - friends[left][0] >= d:
+        left += 1
+    current_sum = prefix[right+1] - prefix[left]
+    if current_sum > max_friendship:
+        max_friendship = current_sum
+
+print(max_friendship)
