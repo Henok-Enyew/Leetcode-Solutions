7ab822e509c8986e50e61ad5cd7f302415c327aa
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-25 22:07:51 +0300

    Created 'Evaluate Reverse Polish Notation' by A2SV Hub

---
+++ 25-Apr-2025/Evaluate Reverse Polish Notation 342645.py
@@ -0,0 +1,22 @@
+# Problem: Evaluate Reverse Polish Notation - https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        opr = set(['+', '-', '*', '/'])
+        for t in tokens:
+            if t in opr:
+                op1 = int(stack.pop())
+                op2 = int(stack.pop())
+                if t == '+':
+                    stack.append(op1 + op2)
+                elif t == '-':
+                    stack.append(op2 - op1)
+                elif t == '*':
+                    stack.append(op1 * op2)
+                elif t == '/':
+                    stack.append(int(op2 / op1))
+                
+            else:
+                stack.append(t)
+        return int(stack[0])
