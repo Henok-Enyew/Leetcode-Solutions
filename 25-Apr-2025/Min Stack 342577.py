6491b3d1ac6b078e7084da885d3128836e2df75c
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-25 21:34:25 +0300

    Created 'Min Stack' by A2SV Hub

---
+++ 25-Apr-2025/Min Stack 342577.py
@@ -0,0 +1,36 @@
+# Problem: Min Stack - https://leetcode.com/problems/min-stack/
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack:
+            self.min_stack.append(val)
+        else:
+            if val <= self.min_stack[-1]:
+                self.min_stack.append(val)
+            
+
+    def pop(self) -> None:
+        popped = self.stack.pop()
+        if popped == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
