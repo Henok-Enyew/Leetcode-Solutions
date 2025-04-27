2ba448b940175a2064581c75da406a47a64ac3af
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-04-27 12:05:44 +0300

    Created 'Broken Keyboard' by A2SV Hub

---
+++ 27-Apr-2025/Broken Keyboard 343293.py
@@ -0,0 +1,15 @@
+# Problem: Broken Keyboard - https://codeforces.com/problemset/problem/1251/A
+
+t = int(input())
+for _ in range(t):
+    s = input().strip()
+    working = set()
+    n = len(s)
+    i = 0
+    while i < n:
+        if i == n - 1 or s[i] != s[i+1]:
+            working.add(s[i])
+            i += 1
+        else:
+            i += 2
+    print(''.join(sorted(working)))
