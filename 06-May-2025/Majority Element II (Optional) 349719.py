8db2ab26a84d77c485edf5bee7084db2580ae1fd
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-05-06 22:34:49 +0300

    Created 'Majority Element II (Optional)' by A2SV Hub

---
+++ 06-May-2025/Majority Element II (Optional) 349719.py
@@ -0,0 +1,11 @@
+# Problem: Majority Element II (Optional) - https://leetcode.com/problems/majority-element-ii/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        map = collections.Counter(nums)
+        output  = []
+        #
+        for key in map:
+            if map[key]  > len(nums) / 3:
+                output.append(key) 
+        return output
