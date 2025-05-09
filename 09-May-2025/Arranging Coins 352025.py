e7ab14ad51bc4ec730f7198a601adca655338182
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-05-09 23:07:20 +0300

    Created 'Arranging Coins' by A2SV Hub

---
+++ 09-May-2025/Arranging Coins 352025.py
@@ -0,0 +1,16 @@
+# Problem: Arranging Coins - https://leetcode.com/problems/arranging-coins/description/
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        # return math.floor(-0.5 + math.sqrt((2*n) + 0.25))
+        left, right = 0, n
+        ans = float('-inf')
+        while left <= right:
+            mid = (left + right) // 2
+            sm = (mid + 1) * (mid) / 2
+            if sm <= n:
+                ans = max(ans, mid)
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
