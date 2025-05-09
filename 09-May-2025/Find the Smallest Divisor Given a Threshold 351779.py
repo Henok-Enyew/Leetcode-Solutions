4cbbbfd3cba396c206494576ada80675a265f637
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-05-09 20:48:35 +0300

    Created 'Find the Smallest Divisor Given a Threshold' by A2SV Hub

---
+++ 09-May-2025/Find the Smallest Divisor Given a Threshold 351779.py
@@ -0,0 +1,17 @@
+# Problem: Find the Smallest Divisor Given a Threshold - https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
+
+class Solution:
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        l,r = 1,max(nums)
+        res = r
+        while l<=r:
+            mid = (l+r) // 2
+            sm =0
+            for n in nums:
+                sm += math.ceil(n/mid)
+            if sm <= threshold:
+                res = min(res,mid)
+                r = mid-1
+            else:
+                l = mid+1
+        return res
