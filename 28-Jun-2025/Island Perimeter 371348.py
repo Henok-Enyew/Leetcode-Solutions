c253235b16ea3c945c8436a6d2580ca35097176f
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-06-28 10:48:03 +0300

    Created 'Island Perimeter' by A2SV Hub

---
+++ 28-Jun-2025/Island Perimeter 371348.py
@@ -0,0 +1,27 @@
+# Problem: Island Perimeter - https://leetcode.com/problems/island-perimeter/description/
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
+        perimeter = 0
+
+        def dfs(r, c):
+            nonlocal perimeter
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == 0:
+                perimeter += 1
+                return
+            if visited[r][c]:
+                return
+            visited[r][c] = True
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    dfs(r, c)
+                    return perimeter 
+
+        return 0
