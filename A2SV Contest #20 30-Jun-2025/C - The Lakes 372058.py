9ff243f7427ab8c0c8aadb7fa8a4bff8b1257d25
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-06-30 19:05:57 +0300

    Created 'C - The Lakes' by A2SV Hub

---
+++ A2SV Contest #20 30-Jun-2025/C - The Lakes 372058.py
@@ -0,0 +1,41 @@
+# Problem: C - The Lakes - https://codeforces.com/gym/618729/problem/C
+
+import sys
+from collections import deque
+
+input = sys.stdin.read().split()
+ptr = 0
+t = int(input[ptr])
+ptr += 1
+for _ in range(t):
+    n, m = map(int, input[ptr:ptr+2])
+    ptr += 2
+    grid = []
+    for _ in range(n):
+        row = list(map(int, input[ptr:ptr+m]))
+        ptr += m
+        grid.append(row)
+    
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    max_volume = 0
+    
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] > 0 and not visited[i][j]:
+                stack = [(i, j)]
+                visited[i][j] = True
+                volume = 0
+                
+                while stack:
+                    x, y = stack.pop()
+                    volume += grid[x][y]
+                    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < n and 0 <= ny < m:
+                            if grid[nx][ny] > 0 and not visited[nx][ny]:
+                                visited[nx][ny] = True
+                                stack.append((nx, ny))
+                
+                max_volume = max(max_volume, volume)
+    
+    print(max_volume)
