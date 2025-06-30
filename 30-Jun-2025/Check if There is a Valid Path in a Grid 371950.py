1d1b969034ad636b99ff5aec5678f54c0d286e1f
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-06-30 09:33:30 +0300

    Created 'Check if There is a Valid Path in a Grid' by A2SV Hub

---
+++ 30-Jun-2025/Check if There is a Valid Path in a Grid 371950.py
@@ -0,0 +1,35 @@
+# Problem: Check if There is a Valid Path in a Grid - https://leetcode.com/problems/check-if-there-is-a-valid-path-in-a-grid/
+
+# from collections import defaultdict
+class Solution:
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+
+        directions = {1: [(0,-1),(0,1)],
+                    2: [(-1,0),(1,0)],
+                    3: [(0,-1),(1,0)],
+                    4: [(0,1),(1,0)],
+                    5: [(0,-1),(-1,0)],
+                    6: [(0,1),(-1,0)]}
+        visited = set([(0,0)])
+
+        def inbound(row, col):
+            return (0 <= row < len(grid) and 0 <= col < len(grid[0]))
+        def dfs(row,col):
+            #base case :)
+            if row == len(grid) - 1 and col == len(grid[0])-1:
+                return True
+
+            for row_change, col_change in directions[grid[row][col]]:
+                new_row = row + row_change
+                new_col = col + col_change
+                if (inbound(new_row, new_col) and
+                (new_row, new_col) not in visited and
+                (-row_change, -col_change) in
+                directions[grid[new_row][new_col]]):
+                    visited.add((new_row, new_col))
+                    found = dfs(new_row, new_col)
+                    if found:
+                        return True
+                    # return False 
+            return False
+        return dfs(0,0)
