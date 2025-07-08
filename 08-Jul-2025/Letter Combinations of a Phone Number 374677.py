2b5a5f54292e13ecbc79b5606c62d0d50775fd45
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-07-08 23:12:51 +0300

    Created 'Letter Combinations of a Phone Number' by A2SV Hub

---
+++ 08-Jul-2025/Letter Combinations of a Phone Number 374677.py
@@ -0,0 +1,29 @@
+# Problem: Letter Combinations of a Phone Number - https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "":
+            return []
+        phone_keypad = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],  
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],  
+        }
+        res = []
+        
+        def backtrack(ind,comb):
+            if len(comb) == len(digits):
+                res.append(comb)
+                return
+            for i in range(len(phone_keypad[digits[ind]])):
+                comb = comb + phone_keypad[digits[ind]][i]
+                backtrack(ind+1, comb)
+                comb = comb[:-1]
+        #
+        backtrack(0,'')
+        return res
