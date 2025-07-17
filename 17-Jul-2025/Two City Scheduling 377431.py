5ac2b21f769a5516b99a38e77abeb11afdf7520b
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-07-17 21:45:25 +0300

    Created 'Two City Scheduling' by A2SV Hub

---
+++ 17-Jul-2025/Two City Scheduling 377431.py
@@ -0,0 +1,14 @@
+# Problem: Two City Scheduling - https://leetcode.com/problems/two-city-scheduling/
+
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        n = len(costs)
+        ans = 0
+        l =  sorted(costs, key=lambda x:(x[0]-x[1])) # Sorting happens Here
+        for i in range(n):
+            if i >= n//2:
+                ans += l[i][1]
+            else:
+                ans += l[i][0]
+#
+        return ans
