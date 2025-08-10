9568ded260daa5ff8fefa12ad3c0b2a9009c58f2
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-08-10 17:31:00 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 10-Aug-2025/Unique Paths 383316.py
@@ -0,0 +1,17 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        @cache
+        def uniquePathsRec(coord):
+            row , col = coord
+            if coord == (m - 1,n - 1):
+                return 1
+            ways = 0
+            if row + 1 < m:
+                ways += uniquePathsRec((row + 1, col))
+            if col + 1 < n:
+                ways += uniquePathsRec((row, col + 1))
+            return ways
+
+        return uniquePathsRec((0,0))
