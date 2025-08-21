905a58aeb5e6556b243a719fafbdc192b5fbb30f
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-08-21 22:20:10 +0300

    Created 'Single Number III' by A2SV Hub

---
+++ 21-Aug-2025/Single Number III 387637.py
@@ -0,0 +1,16 @@
+# Problem: Single Number III - https://leetcode.com/problems/single-number-iii/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        for num in nums:
+            xor ^= num
+        diff_bit = xor & -xor
+        x = 0
+        y = 0
+        for num in nums:
+            if num & diff_bit:
+                x ^= num
+            else:
+                y ^= num
+        return [x, y]
