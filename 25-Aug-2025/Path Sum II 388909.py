a7a6bbbe58e02b5258d2954e4ed3e62d7b1fa770
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-08-25 22:55:04 +0300

    Created 'Path Sum II' by A2SV Hub

---
+++ 25-Aug-2025/Path Sum II 388909.py
@@ -0,0 +1,29 @@
+# Problem: Path Sum II - https://leetcode.com/problems/path-sum-ii/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        def dfs(rt, arr):
+            arr.append(rt.val)
+            if not rt.left and not rt.right:
+                if sum(arr) == targetSum:
+                    res.append(arr.copy())
+                    return
+                 
+            if rt.left:
+                dfs(rt.left, arr)
+                arr.pop()
+            if rt.right:
+                dfs(rt.right, arr)
+                arr.pop()
+
+        dfs(root, [])
+        return res
