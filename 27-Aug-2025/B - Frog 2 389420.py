fa4e0ad4b08a7793cb3d360c066c4299d117bd0e
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-08-27 23:20:13 +0300

    Created 'B - Frog 2' by A2SV Hub

---
+++ 27-Aug-2025/B - Frog 2 389420.py
@@ -0,0 +1,21 @@
+# Problem: B - Frog 2 - https://atcoder.jp/contests/dp/tasks/dp_b
+
+def main():
+    import sys
+    input = sys.stdin.readline
+    
+    N, K = map(int, input().split())
+    h = list(map(int, input().split()))
+
+    dp = [float('inf')] * N
+    dp[0] = 0
+
+    for i in range(1, N):
+        for j in range(1, K + 1):
+            if i - j >= 0:
+                dp[i] = min(dp[i], dp[i - j] + abs(h[i] - h[i - j]))
+    
+    print(dp[N - 1])
+
+if __name__ == "__main__":
+    main()
