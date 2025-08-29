b960a7565d64a484828711a7f5a0f47741e39247
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-08-29 21:03:21 +0300

    Created 'Minimum Replacements to Sort the Array' by A2SV Hub

---
+++ 29-Aug-2025/Minimum Replacements to Sort the Array 389907.py
@@ -0,0 +1,21 @@
+# Problem: Minimum Replacements to Sort the Array - https://leetcode.com/problems/minimum-replacements-to-sort-the-array/
+
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        N = len(nums)
+        total_opt = 0
+        prev_value = nums[-1]
+
+        for index in range(N - 2, -1, -1):
+            new_value = nums[index]
+            
+            if new_value <= prev_value:
+                prev_value = nums[index]
+                continue
+            curr_opt =ceil( new_value /prev_value) 
+            prev_value = new_value // curr_opt
+            
+           
+            
+            total_opt += curr_opt - 1
+        return total_opt
