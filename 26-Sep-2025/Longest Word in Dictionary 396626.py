8f0cb022e607c7ae3811ba2f795debbeb2e9a64c
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-09-26 21:43:43 +0300

    Created 'Longest Word in Dictionary' by A2SV Hub

---
+++ 26-Sep-2025/Longest Word in Dictionary 396626.py
@@ -0,0 +1,27 @@
+# Problem: Longest Word in Dictionary - https://leetcode.com/problems/longest-word-in-dictionary/
+
+class TrieNode:
+    def __init__(self, val):
+        self.children = {}
+        self.value = val
+        self.endOfWord = False
+        
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        root = TrieNode(0)
+        maxLen = 0
+        res = ""
+        for word in sorted(words):
+            cur = root
+            count = 0
+            for letter in word:
+                if letter not in cur.children:
+                    cur.children[letter] = TrieNode(count)
+                cur = cur.children[letter]
+                if cur.endOfWord: count += 1
+            cur.endOfWord = True
+            cur.value += 1
+            if cur.value == len(word) and cur.value > maxLen:
+                maxLen = cur.value
+                res = word
+        return res
