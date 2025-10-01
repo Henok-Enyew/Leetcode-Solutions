7bacaa558223e1e4ea079a2f4cb0b5f953619eaf
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-10-01 21:22:52 +0300

    Created 'Network Delay Time' by A2SV Hub

---
+++ 01-Oct-2025/Network Delay Time 398703.py
@@ -0,0 +1,16 @@
+# Problem: Network Delay Time - https://leetcode.com/problems/network-delay-time/description/
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort()
+        res = []
+        for f in folder:
+            if not res:
+                res.append(f)
+            else:
+                prev = res[-1]
+                if f.startswith(prev) and len(f) > len(prev) and f[len(prev)] == '/':
+                    continue
+                else:
+                    res.append(f)
+        return res
