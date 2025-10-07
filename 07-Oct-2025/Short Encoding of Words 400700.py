c91f90c32c865d070df4d37ef18484553600ab7f
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-10-07 22:19:21 +0300

    Created 'Short Encoding of Words' by A2SV Hub

---
+++ 07-Oct-2025/Short Encoding of Words 400700.py
@@ -0,0 +1,10 @@
+# Problem: Short Encoding of Words - https://leetcode.com/problems/short-encoding-of-words/
+
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        words.sort(key=len, reverse=True)
+        res = []
+        for suffix in words:
+            if not any(word.endswith(suffix) for word in res): 
+                res.append(suffix)
+        return sum(len(word)+1 for word in res)  
