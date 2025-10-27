ba2bcfc75bf0e20b963652a85f3d4b96f5081024
Henok Enyew <154423957+Henok-Enyew@users.noreply.github.com>
2025-10-27 23:21:44 +0300

    Created 'Validate Binary Search Tree' by A2SV Hub

---
+++ 27-Oct-2025/Validate Binary Search Tree 406057.py
@@ -0,0 +1,14 @@
+# Problem: Validate Binary Search Tree - https://leetcode.com/problems/validate-binary-search-tree/
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        prev = float('-inf')
+        def inorder(node):
+            nonlocal prev
+            if not node:
+                return True
+            if not (inorder(node.left) and prev < node.val):
+                return False
+            prev = node.val
+            return inorder(node.right)
+        return inorder(root)
